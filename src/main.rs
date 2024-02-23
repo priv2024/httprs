e0979d417ed17f28e5b36114e4a5267790a0c8f8
priv2024 <@priv2024>
2024-02-23 20:39:50 +0100

    use async-channel instead of loome or kanal and also disable idle hosts in pool

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use reqwest::Client;
-use std::sync::Arc;
 use tokio::io::{AsyncBufReadExt, BufReader, Lines, Stdin};
 
 const SCHEME_HTTPS: &str = "https://";
@@ -13,56 +12,60 @@ struct Config {
     threads: usize,
 }
 
+async fn process_host(client: &Client, host: &String) {
+    let mut https_url = String::from(SCHEME_HTTPS);
+    https_url.push_str(host.as_str());
+    if let Ok(_res) = client.get(&https_url).send().await {
+        println!("{}{}", SCHEME_HTTPS, host);
+        return;
+    }
+
+    let mut http_url = String::from(SCHEME_HTTP);
+    http_url.push_str(host.as_str());
+    if let Ok(_res) = client.get(&http_url).send().await {
+        println!("{}{}", SCHEME_HTTP, host);
+    }
+}
+
 async fn process(
     mut lines: Lines<BufReader<Stdin>>,
     config: &Config,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let client = Arc::new(
-        Client::builder()
-            .danger_accept_invalid_certs(true)
-            .timeout(std::time::Duration::from_millis(config.timeout))
-            .redirect(reqwest::redirect::Policy::none())
-            .tcp_keepalive(None)
-            .tcp_nodelay(true)
-            .https_only(false)
-            .user_agent("httprs/0.1.0")
-            .build()
-            .unwrap(),
-    );
+    let client = Client::builder()
+        .danger_accept_invalid_certs(true)
+        .timeout(std::time::Duration::from_millis(config.timeout))
+        .redirect(reqwest::redirect::Policy::none())
+        .tcp_keepalive(None)
+        .tcp_nodelay(true)
+        .https_only(false)
+        .pool_max_idle_per_host(0)
+        .user_agent("httprs/0.1.0")
+        .build()
+        .unwrap();
 
-    let (input_tx, input_rx) = loole::bounded::<String>(config.threads);
+    let (tx, rx) = async_channel::bounded(config.threads);
 
-    let mut tasks = vec![];
+    let mut handles = vec![];
     for _ in 0..config.threads {
         let client = client.clone();
-        let input_rx = input_rx.clone();
+        let rx = rx.clone();
 
-        tasks.push(tokio::spawn(async move {
-            while let Ok(host) = input_rx.recv_async().await {
-                let mut https_url = String::from(SCHEME_HTTPS);
-                https_url.push_str(host.as_str());
-                if let Ok(_res) = client.get(&https_url).send().await {
-                    println!("{}{}", SCHEME_HTTPS, host);
-                    return;
-                }
-
-                let mut http_url = String::from(SCHEME_HTTP);
-                http_url.push_str(host.as_str());
-                if let Ok(_res) = client.get(&http_url).send().await {
-                    println!("{}{}", SCHEME_HTTP, host);
-                }
+        handles.push(tokio::spawn(async move {
+            while let Ok(host) = rx.recv().await {
+                process_host(&client, &host).await
             }
-            input_rx.close();
-        }))
+
+            rx.close();
+        }));
     }
 
-    while let Some(host) = lines.next_line().await? {
-        input_tx.send_async(host).await.unwrap();
+    while let Some(line) = lines.next_line().await.unwrap() {
+        tx.send(line).await.unwrap()
     }
 
-    input_tx.close();
+    tx.close();
 
-    for task in tasks {
+    for task in handles {
         task.await.unwrap();
     }
 
@@ -73,7 +76,7 @@ async fn process(
 async fn main() {
     let config = Config {
         timeout: 5000,
-        threads: 600,
+        threads: 100,
     };
 
     let stdin = tokio::io::stdin();
